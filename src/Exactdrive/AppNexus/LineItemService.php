efef65e286cc29608354fffeaf75cd5a08c6ac70
Oliver Milanovic <omilanovic@codeframe.ch>
2016-11-17 13:43:52 +0100

    Improved phpdoc, formatted code

--- src/Exactdrive/AppNexus/LineItemService.php
+++ src/Exactdrive/AppNexus/LineItemService.php
@@ -75,7 +75,8 @@ class LineItemService extends Api
      */
     public static function getBaseUrl()
     {
-        $url = Api::getBaseUrl() . '/line-item';
+        $url = Api::getBaseUrl().'/line-item';
+
         return $url;
     }
 
@@ -84,27 +85,30 @@ class LineItemService extends Api
     /**
      * Add a new line-item.
      *
-     * @param  int  $advertiserId => Advertiser id of line item.
-     * @param  hash $lineItem     => Only valid fields will be passed to api.
-     * @return hash $lineItem     => Newly created appnexus line item.
+     * @param  int $advertiserId => Advertiser id of line item.
+     * @param  array $lineItem => Only valid fields will be passed to api.
+     *
+     * @return AppNexusObject $lineItem     => Newly created appnexus line item.
      */
-    public static function addLineItem($advertiserId, $lineItem)
+    public static function addLineItem( $advertiserId, $lineItem )
     {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'advertiser_id' => $advertiserId
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'advertiser_id' => $advertiserId,
+                )
+            );
 
         // package up the data, don't bother running query on invalid data
-        $data = self::_createLineItemHash($lineItem);
+        $data = self::_createLineItemHash( $lineItem );
         if ($data == null) {
             return null;
         }
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::POST, $data);
+        $response = self::makeRequest( $url, Api::POST, $data );
 
-        return new AppNexusObject($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusObject( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
@@ -112,29 +116,32 @@ class LineItemService extends Api
     /**
      * Update an existing line item.
      *
-     * @param  int  $id           => Id of line item.
-     * @param  int  $advertiserId => Advertiser id of line item.
-     * @param  hash $lineItem     => Only valid fields will be passed to api.
-     * @return hash $lineItem     => Updated appnexus line item.
+     * @param  int $id => Id of line item.
+     * @param  int $advertiserId => Advertiser id of line item.
+     * @param  array $lineItem => Only valid fields will be passed to api.
+     *
+     * @return AppNexusObject $lineItem     => Updated appnexus line item.
      */
-    public static function updateLineItem($id, $advertiserId, $lineItem)
+    public static function updateLineItem( $id, $advertiserId, $lineItem )
     {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'id'            => $id,
-            'advertiser_id' => $advertiserId
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'id'            => $id,
+                    'advertiser_id' => $advertiserId,
+                )
+            );
 
         // package up the data, don't bother running query on invalid data
-        $data = self::_createLineItemHash($lineItem);
+        $data = self::_createLineItemHash( $lineItem );
         if ($data == null) {
             return null;
         }
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::PUT, $data);
+        $response = self::makeRequest( $url, Api::PUT, $data );
 
-        return new AppNexusObject($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusObject( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
@@ -142,53 +149,63 @@ class LineItemService extends Api
     /**
      * View all line items for an advertiser, results are paged.
      *
-     * @param  int   $advertiserId
-     * @return array $lineItems
+     * @param int $advertiserId
+     * @param int $start_element
+     * @param int $num_elements
+     *
+     * @return AppNexusArray $lineItems
      */
-    public static function getAllLineItems($advertiserId,
-        $start_element = 0, $num_elements = 100)
-    {
+    public static function getAllLineItems(
+        $advertiserId,
+        $start_element = 0,
+        $num_elements = 100
+    ) {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'advertiser_id' => $advertiserId,
-            'start_element' => $start_element,
-            'num_elements'  => $num_elements
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'advertiser_id' => $advertiserId,
+                    'start_element' => $start_element,
+                    'num_elements'  => $num_elements,
+                )
+            );
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::GET);
+        $response = self::makeRequest( $url, Api::GET );
 
         // wrap response with app nexus object
-        return new AppNexusArray($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusArray( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
 
     /**
-     * View line items speficied by ids, results are paged.
+     * View line items specified by ids, results are paged.
      *
-     * @param  array(int) $ids
-     * @return array      $lineItems
+     * @param  int[] $ids
+     *
+     * @return array|AppNexusArray
      */
-    public static function getLineItems($ids)
+    public static function getLineItems( $ids )
     {
         // [moiz] need to fix this...
 
         // shortcut if only single id is specified
-        if (count($ids) == 1) {
-            return array(self::getLineItem($ids[0]));
+        if (count( $ids ) == 1) {
+            return array( self::getLineItem( $ids[0] ) );
         }
 
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'id' => implode(',', $ids)
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'id' => implode( ',', $ids ),
+                )
+            );
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::GET);
+        $response = self::makeRequest( $url, Api::GET );
 
         // wrap response with app nexus object
-        return new AppNexusArray($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusArray( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
@@ -196,21 +213,24 @@ class LineItemService extends Api
     /**
      * View a specific line item.
      *
-     * @param  int  $id
-     * @return hash $lineItem
+     * @param  int $id
+     *
+     * @return AppNexusObject $lineItem
      */
-    public static function getLineItem($id)
+    public static function getLineItem( $id )
     {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'id' => $id
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'id' => $id,
+                )
+            );
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::GET);
+        $response = self::makeRequest( $url, Api::GET );
 
         // wrap response with app nexus object
-        return new AppNexusObject($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusObject( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
@@ -219,24 +239,31 @@ class LineItemService extends Api
      * Search for line items with ids or names containing certain characters,
      *  results are paged.
      *
-     * @param  string $term
-     * @return array  $lineItems
+     * @param string $term
+     * @param int $start_element
+     * @param int $num_elements
+     *
+     * @return AppNexusArray $lineItems
      */
-    public static function searchLineItems($term,
-        $start_element = 0, $num_elements = 100)
-    {
+    public static function searchLineItems(
+        $term,
+        $start_element = 0,
+        $num_elements = 100
+    ) {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'search'        => $term,
-            'start_element' => $start_element,
-            'num_elements'  => $num_elements
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'search'        => $term,
+                    'start_element' => $start_element,
+                    'num_elements'  => $num_elements,
+                )
+            );
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::GET);
+        $response = self::makeRequest( $url, Api::GET );
 
         // wrap response with app nexus object
-        return new AppNexusArray($response, AppNexusObject::MODE_READ_WRITE);
+        return new AppNexusArray( $response, AppNexusObject::MODE_READ_WRITE );
     }
 
     //-------------------------------------------------------------------------
@@ -244,20 +271,23 @@ class LineItemService extends Api
     /**
      * Delete a line item.
      *
-     * @param  int  $id           => Id of line item.
-     * @param  int  $advertiserId => Advertiser id of line item.
+     * @param  int $id => Id of line item.
+     * @param  int $advertiserId => Advertiser id of line item.
+     *
      * @return bool $status
      */
-    public static function deleteLineItem($id, $advertiserId)
+    public static function deleteLineItem( $id, $advertiserId )
     {
         // construct url
-        $url = self::getBaseUrl() . '?' . http_build_query(array(
-            'id'            => $id,
-            'advertiser_id' => $advertiserId
-        ));
+        $url = self::getBaseUrl().'?'.http_build_query(
+                array(
+                    'id'            => $id,
+                    'advertiser_id' => $advertiserId,
+                )
+            );
 
         // query app nexus server
-        $response = self::makeRequest($url, Api::DELETE);
+        self::makeRequest( $url, Api::DELETE );
 
         return true;
     }
@@ -270,31 +300,32 @@ class LineItemService extends Api
      * Returns a line item hash containing only the fields which are allowed
      *  to be updated in the format accepted by AppNexus.
      *
-     * @param  hash $lineItem
-     * @return hash $lineItem
+     * @param  array $lineItem
+     *
+     * @return array|null|object
      */
-    private static function _createLineItemHash($lineItem)
+    private static function _createLineItemHash( $lineItem )
     {
-        if (is_object($lineItem)) {
-            $pruned = new stdClass();
+        if (is_object( $lineItem )) {
+            $pruned = new \stdClass();
             foreach (self::$fields as $key) {
-                if (property_exists($lineItem, $key)) {
+                if (property_exists( $lineItem, $key )) {
                     $pruned->$key = $lineItem->$key;
                 }
             }
 
             // return null if no valid fields found
-            return empty($pruned) ? null : (object) array('line-item' => $pruned);
+            return empty( $pruned ) ? null : (object) array( 'line-item' => $pruned );
         } else {
             $pruned = array();
             foreach (self::$fields as $key) {
-                if (array_key_exists($key, $lineItem)) {
+                if (array_key_exists( $key, $lineItem )) {
                     $pruned[$key] = $lineItem[$key];
                 }
             }
 
             // return null if no valid fields found
-            return empty($pruned) ? null : array('line-item' => $pruned);
+            return empty( $pruned ) ? null : array( 'line-item' => $pruned );
         }
     }
 
